4e855de7058e53b85774238091bd9171cd5076c7
霍英杰 <632307060630@mails.cqjtu.edu.cn>
2026-07-07 09:03:39 +0800

    Add Android mobile app client

---
+++ android-app/app/src/main/java/com/smartlighting/app/Constants.java
@@ -0,0 +1,11 @@
+package com.smartlighting.app;
+
+public final class Constants {
+    public static final String BASE_URL = "http://d5dfca47.natappfree.cc";
+    public static final String PREFS = "smart_lighting_mobile";
+    public static final String KEY_TOKEN = "token";
+    public static final String KEY_USERNAME = "username";
+
+    private Constants() {
+    }
+}
